553b8f6c41664d859616c2b0e02d766e1ffe18fa
camille <camille.lavarde@edu.devinci.fr>
2021-10-21 16:39:53 -0400

    init Tp2

---
+++ TP2/DataTransmitter/DataTransmitter.cpp
@@ -0,0 +1,12 @@
+﻿// DataTransmitter.cpp : définit le point d'entrée de l'application.
+//
+
+#include "DataTransmitter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
